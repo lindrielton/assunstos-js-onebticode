f3ce0c38e76b8f02a0efbf2cf4a84de2c91fde5f
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2026-04-07 05:52:10 -0300

    finalizando

--- assuntos/09-tentando-sozinho/exercico.js
+++ assuntos/09-tentando-sozinho/exercico.js
@@ -54,3 +54,52 @@ console.log(mediaPonderada({ n: 7, p: 2}, { n: 9, p: 5 }, { n: 3, p: 1 }))
 
 //mediana(15, 14, 8, 7, 3) === 8
 // porque 8 está no centro da sequência
+
+
+let mediana = (...lista) => {
+   let order = [...lista].sort((a, b) => a - b )
+   let division = Math.floor(order.length / 2) 
+   if(order.length % 2 !== 0){
+      return `só tem um numero no centro ${order[division]}` 
+   }
+   const numparent = order[division - 1]
+   const numlist = order[division]
+   return mediaSimples(numparent, numlist)
+}
+
+console.log(`media = ${mediana(10,8,7,6,5,3)}`)
+console.log(`media = ${mediana(2, 4, 5, 7, 42, 99)} `)
+
+
+// Moda 
+
+//- **Moda:** Pode ser calculada encontrando o 
+// valor que mais se repete em um dado conjunto.
+    
+// Exemplo:
+
+//moda(1, 1, 5, 4, 9, 7, 4, 3, 5, 2, 4, 0, 4) === 4
+// porque:
+// 4 aparece 4 vezes
+// 5 e 1 aparecem 2 vezes
+// 9, 7, 3, 2 e 0 aparecem 1 vez
+
+
+let moda = (...list) => {
+   let lista = list.map((num)=>[
+      num,
+      list.filter((n) => n === num).length
+      
+   ])
+ 
+   lista.sort((a, b) =>  b[1] - a[1])
+   console.log(lista)
+   return lista[0][0]
+   
+   
+}
+
+
+
+
+console.log(`${moda(1, 1, 5, 4, 9, 7, 4, 3, 5, 2, 4, 0, 4)}`)
