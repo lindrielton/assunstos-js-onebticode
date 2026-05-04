0ae26adb3f7682cac1aee346bfe2adf2d870203c
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2026-05-04 12:00:30 -0300

    tentando resolver

--- assuntos/16-exercicio-Datas-com-daysjs/index.js
+++ assuntos/16-exercicio-Datas-com-daysjs/index.js
@@ -1,20 +1,33 @@
 
-function dataactual(){
+function dataactual(date){
+   let daysjs = require("dayjs")
+   const relativeTime = require('dayjs/plugin/relativeTime')
+   daysjs.extend(relativeTime)
 
-    let daysjs = require("dayjs")
-    const relativeTime = require('dayjs/plugin/relativeTime')
-    daysjs.extend(relativeTime)
-    let checando = daysjs('1970-00-00', 'YYYY-MM-DD').isValid()// se e valido
-    let padranbrasilian = daysjs('2019-01-25').format('DD/MM/YYYY')// passando para o formato brasileiro
-    let dateAtual = daysjs('1999-01-01').fromNow()// calculando a idade
+  
+    let checando = daysjs(date, 'YYYY-MM-DD').isValid()// se e valido
+    let anoaActual =  new Date()
+   
+    if(checando === true){
+          let padranbrasilian = daysjs(date).format('DD/MM/YYYY')// passando para o formato brasileiro
+          let dateAtual = daysjs(date).fromNow()// calculando a idade
+          let datNascimento = daysjs(date)
+          let dayrestant = datNascimento.diff(anoaActual, 'year')
+           console.log(`sua idade atual é   : ${dateAtual} : no padrão brasileiro fica assim : ${padranbrasilian} `)  
+           
+    }if(datNascimento < anoaActual){
+         
+    }
+    
+    
+        
+        
+       
+    
 
-        return {
-            checando,
-            padranbrasilian,
-            dateAtual
-        }
+      
 
 }
 
 
-console.log(dataactual())
+console.log(dataactual('04-25-1999'))
