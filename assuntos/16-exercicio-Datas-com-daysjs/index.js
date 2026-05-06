e6f4b848e298b41c0ce648fa36bcdd8d3deae72b
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2026-05-05 22:21:57 -0300

    utilizando dayjs

--- assuntos/16-exercicio-Datas-com-daysjs/index.js
+++ assuntos/16-exercicio-Datas-com-daysjs/index.js
@@ -1,33 +1,29 @@
 
-function dataactual(date){
-   let daysjs = require("dayjs")
-   const relativeTime = require('dayjs/plugin/relativeTime')
-   daysjs.extend(relativeTime)
+
+   const  dayjs = require("dayjs")
+   
 
   
-    let checando = daysjs(date, 'YYYY-MM-DD').isValid()// se e valido
-    let anoaActual =  new Date()
-   
-    if(checando === true){
-          let padranbrasilian = daysjs(date).format('DD/MM/YYYY')// passando para o formato brasileiro
-          let dateAtual = daysjs(date).fromNow()// calculando a idade
-          let datNascimento = daysjs(date)
-          let dayrestant = datNascimento.diff(anoaActual, 'year')
-           console.log(`sua idade atual é   : ${dateAtual} : no padrão brasileiro fica assim : ${padranbrasilian} `)  
-           
-    }if(datNascimento < anoaActual){
-         
+    
+    function birthday(date){
+        const birthday = dayjs(date)
+        const today = dayjs()
+
+       //pegando a diferença da data atual para a data da pessoa que vai resultar na idade da pessoa
+        const dateAcutual = today.diff(birthday, 'year')
+       //adicinando a idade que a pessoa tem na data de nascimento  somando mais um, isso vai resultar no prooximo ano
+        const  nextBirthday = birthday.add(dateAcutual + 1, 'year')
+        // pegando os dias que faltão para o proximo aniversario, somamdo + 1 para que ele não arredonte um aniversario 
+        // no dia atual para 0
+        const daystoNextBorthday = nextBirthday.diff(today, 'day') + 1
+
+
+        console.log(`idade: ${dateAcutual} `)
+        console.log(`proximo aniversário ${nextBirthday.format('DD/MM/YYYY')}`)
+        console.log(`faltão ${daystoNextBorthday} dias para o seu aniversário`)
     }
     
-    
-        
-        
-       
-    
-
-      
-
-}
 
 
-console.log(dataactual('04-25-1999'))
+
+console.log(birthday('1999-04-25'))
