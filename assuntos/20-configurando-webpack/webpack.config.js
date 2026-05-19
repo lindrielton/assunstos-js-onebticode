089289401e540c869df95fc563100cac5bf37086
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2026-05-19 06:10:09 -0300

    finalizando

--- assuntos/20-configurando-webpack/webpack.config.js
+++ assuntos/20-configurando-webpack/webpack.config.js
@@ -1,5 +1,16 @@
+
+const path = require('path')
+
+
+
 module.exports = {
     entry:{
-        index: './src/index.js' //identificando entrada
-    }
+        main: './src/index.js', //identificando entrada
+        hellow: './src/hellow.js'
+    },
+        mode:'production',
+        output:{
+            path: path.resolve(__dirname, 'public'),
+            filename: '[name].bundle.min.js'
+        }
 }
